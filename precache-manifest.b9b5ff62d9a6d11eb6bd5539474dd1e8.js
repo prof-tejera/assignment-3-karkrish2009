beebe951a1db69a1e541215ba502b78eec43ba65
karkrish2009 <karkrish2009@users.noreply.github.com>
2022-12-19 01:39:33 +0000

    deploy: 6d4ea58193551d8a9f34fc4a0eb12acc9ccb3931

---
+++ precache-manifest.b9b5ff62d9a6d11eb6bd5539474dd1e8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "9bcec98fec3d61c89332",
+    "url": "/assignment-3-karkrish2009/static/css/main.4b13e727.chunk.css"
+  },
+  {
+    "revision": "9bcec98fec3d61c89332",
+    "url": "/assignment-3-karkrish2009/static/js/main.9bcec98f.chunk.js"
+  },
+  {
+    "revision": "a96734f1aa8b27447a4d",
+    "url": "/assignment-3-karkrish2009/static/js/1.a96734f1.chunk.js"
+  },
+  {
+    "revision": "8158782f0198af254aca",
+    "url": "/assignment-3-karkrish2009/static/js/2.8158782f.chunk.js"
+  },
+  {
+    "revision": "324cfc0fb803dffbefd2",
+    "url": "/assignment-3-karkrish2009/static/js/runtime~main.324cfc0f.js"
+  },
+  {
+    "revision": "61968eac5548af24550b4172f5823695",
+    "url": "/assignment-3-karkrish2009/index.html"
+  }
+];
